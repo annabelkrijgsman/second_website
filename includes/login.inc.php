498bd8a0f6d6dd3bfbf56fd04e8e90b768e0d5c2
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-10 16:41:45 +0100

    login

---
+++ includes/login.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+if (isset($_POST['loginsubmit'])) {
+    
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+    
+    if (empty($email) || empty($pwd)){
+        header("Location: ../login.php?error=emptyfields");
+        exit();        
+    }
+    else {
+        $sql = "SELECT * FROM Users WHERE Username=?;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../login.php?error=sqlerror");
+        exit();            
+        }
+        else {
+            
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if ($row = mysqli_fetch_assoc($result)) {
+                $pwdcheck = password_verify($pwd, $row['Password']);
+                if ($pwdcheck == false) {
+                    header("Location: ../login.php?error=wrongpwd");
+                    exit();                        
+                }
+                elseif ($pwdcheck == true) {
+                    session_start();
+                    $_SESSION['userID'] = $row['ID'];
+                    $_SESSION['userUsername'] = $row['Username'];
+                    header("Location: account.php?login=succes");
+                    exit();
+                }
+                else {
+                    header("Location: ../login.php?error=wrongpwd");
+                    exit();                     
+                }
+            }
+            else {
+                header("Location: ../login.php?error=nouser");
+                exit();                
+            }
+            
+        }
+    }
+    
+}   
+    else {
+        header("Location: ../login.php");
+        exit();
+    }
+    
+?>
