21d33fc701bd3e1f33e57684bb186f75b9753bad
annabelkrijgsman <44801054+annabelkrijgsman@users.noreply.github.com>
2018-12-12 13:58:17 +0100

    not sure if I changed anything

--- login.php
+++ login.php
@@ -4,12 +4,15 @@
         
         <article>
             
-            <form method="post" id="contactblock" action="includes/login.inc.php">
+            <!--LOGIN FORM-->
+            <h2 align="center">Login</h2>
+            <form method="post" id="form" action="includes/login.inc.php">
                 <label for="uname">Username</label><br/>
                 <input type="text" class="textfield" name="email" placeholder="Username.."><br/>
                 <label for="uname">Password</label><br/>
                 <input type="password" class="textfield" name="pwd" placeholder="Username.."><br/>
-                <button type="submit" name="loginsubmit" class="button">Login</button>
+                <button type="submit" name="loginsubmit" class="button">Login</button><br/>
+                <a href="signup.php">Don't have an account yet?</a>
             </form>
             
         </article>
