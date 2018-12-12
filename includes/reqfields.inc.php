2cb47c4799792e3bfdb379352dd42976ee1ff231
annabelkrijgsman <44801054+annabelkrijgsman@users.noreply.github.com>
2018-12-12 14:04:37 +0100

    required fields for writing an advert

---
+++ includes/reqfields.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+//REQUIRED FIELDS
+$titleErr = $advertErr = $priceErr = "";
+$title = $advert = $price = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["title"])) {
+			$titleErr = "Title is required";
+	}
+    	
+	if (empty($_POST["advert"])) {
+			$advertErr = "Advert is required";
+	}
+
+	if (empty($_POST["price"])) {
+			$priceErr = "Price is required";
+	}
+	
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+		}
+	}
+	
+?>
