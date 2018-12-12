3f573f03b181379eebdbd4e7de5fa88c22cc3120
annabelkrijgsman <44801054+annabelkrijgsman@users.noreply.github.com>
2018-12-12 14:02:58 +0100

    show some different stuff when logged in

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,11 +1,55 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
     
     <head>
         <title>Marktplaats</title>
+        <link rel="stylesheet" type="text/css" href="includes/marktplaats.css" />
+        
+        <?php
+            require 'connection.inc.php';
+        ?>
+        
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+        
     </head>
     
     <body>
+        
         <header>
             
+            <div class="navigation">
+                    <ul class="nav">
+                        <li><a href="index.php">Home</a></li>
+                        
+                            <?php
+                            
+                                if (isset($_SESSION['userID'])) {
+                                    echo '<li><a href="account.php" style="color:green">Account</a></li>';
+                                }
+                                else {
+                                    echo '<li><a href="login.php">Login/Sign Up</a></li>';
+                                }
+                                
+                               if (isset($_SESSION['userID'])) {
+                                    //DOESN'T LOGOUT YET
+                                    echo '<form method="post" id="contactblock" action="includes/logout.inc.php">';
+                                    echo '<li><button type="submit" name="logoutsubmit" class="button">Log Out</button></li>';
+                                    echo '</form>';
+                                }
+                                
+                            ?>
+                    </ul>
+            </div>
+            <div class="search">
+                    <ul class="nav">
+                        <!--SEARCHFIELD + CATEGORIES-->
+                    </ul>
+            </div>
+            <br/>
         </header>
