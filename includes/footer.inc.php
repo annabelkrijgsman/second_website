a01e81540eeece97d5a26e84be6d6fcc2f20cd96
annabelkrijgsman <44801054+annabelkrijgsman@users.noreply.github.com>
2018-12-14 11:39:09 +0100

    added footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,18 @@
-        <footer>
-            
+    
+        <footer>    
+                <div class="bottom">
+                  <br/>
+                  <br/>
+                  &copy; All rights reserved.
+                  <br/>
+                </div>
+                  
+              </div>
+              
+          <?php
+            $connection = null;
+          ?>
+          
         </footer>
-    </body>
-
+	</body>
 </html>
