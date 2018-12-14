4f339277e7cd6f9457a7c012c2df4b7a9fb6ac1f
annabelkrijgsman <44801054+annabelkrijgsman@users.noreply.github.com>
2018-12-14 11:22:57 +0100

    connection

--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -8,9 +8,28 @@ $db = "Marktplaats";
 // CREATE CONNECTION
 $conn = mysqli_connect($host, $user_name, $pass_word, $db);
 
-    // CHECK CONNECTION
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+// CHECK CONNECTION
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+class Database {
+    public static function getConn() {
+        $host = "localhost";
+        $user_name = "root";
+        $pass_word = "";
+        $db = "Marktplaats";
+        
+        // CREATE CONNECTION
+        $conn = mysqli_connect($host, $user_name, $pass_word, $db);
+        
+        // CHECK CONNECTION
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        
+        return $conn;
     }
     
+}
 ?>
